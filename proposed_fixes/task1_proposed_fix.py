6aab07d57f3bae7c6bea1dbcc9e2c869219a93b9
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2025-07-24 13:19:41 +0530

    Auto-fix for task task1

--- proposed_fixes/task1_proposed_fix.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-df = spar.table("model.modelllm.weather_data_jun_12_2025")
-filtered_df = df.filter((df.state == "Bihar") & (df.district == "Araria"))
-display(filtered_df)
