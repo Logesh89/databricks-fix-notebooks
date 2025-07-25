bc1006ccfa220efb2bdd31908e3df92abf5f006c
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2025-07-25 14:34:19 +0530

    Auto-fix for task task3

--- cloned_notebooks/task3.py
+++ cloned_notebooks/task3.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 df = spark.table("model.modelllm.weather_data_jun_12_2025")
-filtered_df = df.filter((df.state == "Bihar") & (df.district == "Araria"))
+filtered_df = df.filter((df.state == "Bihar") & (df.district == "Araria")) 
 display(filtered_df)
