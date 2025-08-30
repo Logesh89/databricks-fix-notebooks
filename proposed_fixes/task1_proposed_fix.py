df03d9a5ce4280bfc8684dd1451d129ec880af93
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2025-08-30 21:19:32 +0530

    Auto-fix for task task1

--- proposed_fixes/task1_proposed_fix.py
+++ proposed_fixes/task1_proposed_fix.py
@@ -1,6 +1,11 @@
+# Databricks notebook source
 df = spark.table("model.modelllm.weather_data_jun_12_2025")
 filtered_df = df.filter((df.state == "Bihar") & (df.district == "Aurangabad")) 
 display(filtered_df)
+
+
+
+# COMMAND ----------
 
 df = spark.table("model.modelllm.weather_data_jun_12_2025")
 filtered_df = df.filter((df.state == "Bihar") & (df.district == "Arwal")) 
