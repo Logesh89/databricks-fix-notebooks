ad835beab4abdbd2d39313c47eda1cc8569515a7
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2025-09-15 18:57:01 +0530

    Auto-fix for task task1

--- proposed_fixes/task1_proposed_fix.py
+++ proposed_fixes/task1_proposed_fix.py
@@ -1,7 +1,3 @@
 # Databricks notebook source
 df = spark.table("model.modelllm.weather_data_jun_12_2025")
 display(df)
-
-# COMMAND ----------
-
-df_weather_data_jun_12_2025 = spark.table("model.modelllm.weather_data_jun_12_2025")
