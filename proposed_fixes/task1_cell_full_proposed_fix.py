270030a676834cfe7bd67e59447e653336dac47a
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2025-09-16 13:15:11 +0530

    Auto-fix for task task1

---
+++ proposed_fixes/task1_cell_full_proposed_fix.py
@@ -0,0 +1,32 @@
+df = spark.table("model.modelllm.weather_data_jun_12_2025")
+display(df)
+
+df = spark.table("model.modelllm.weather_data_jun_12_2025")
+filtered_df = df.filter((df.state == "Bihar") & (df.district == "Aurangabad")) 
+display(filtered_df)
+
+df = spark.table("model.modelllm.weather_data_jun_12_2025")
+filtered_df = df.filter((df.state == "Bihar") & (df.district == "Arwal")) 
+display(filtered_df)
+
+# MAGIC %sql
+SELECT date, district, temperatures_min, temperatures_max
+FROM model.modelllm.weather_data_jun_12_2025
+LIMIT 10;
+
+df_bihar = df.filter((df.state == "Bihar") & (df.district == "Araria"))
+display(df_bihar.select("date", "temperatures_min", "temperatures_max", "precipitation_amount"))
+
+from pyspark.sql import functions as F
+
+df_agg = (
+    df.groupBy("district")
+      .agg(
+          F.avg("temperatures_min").alias("avg_min_temp"),
+          F.avg("temperatures_max").alias("avg_max_temp"),
+          F.sum("precipitation_amount").alias("total_rain")
+      )
+      .orderBy(F.col("avg_max_temp").desc())
+)
+
+display(df_agg)
