d722a9a2947ef6756e77d6bb6494bce1387a1a28
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2025-09-17 17:33:23 +0530

    Auto-fix for task Task3

---
+++ cloned_notebooks/Task3.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+df = spark.table("model.modelllm.weather_data_jul_10_2025")
+filtered_df = df.filter((df.state == "Bihar") & (df.district == "Aurangabad")) 
+display(filtered_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Daily Min/Max Temperature Difference
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+
+df_with_diff = df.withColumn(
+    "temp_diff", 
+    F.col("temperatures_max") - F.col("temperatures_min")
+)
+
+display(df_with_diff.select("date", "district", "temperatures_min", "temperatures_max", "temp_diff"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC %md
+# MAGIC ### Hottest Districts (by Max Temp)
+
+# COMMAND ----------
+
+df_hottest = (
+    df.groupBy("district")
+      .agg(F.max("temperatures_max").alias("hottest_temp"))
+      .orderBy(F.col("hottest_temp").desc())
+      .limit(5)
+)
+
+display(df_hottest)
