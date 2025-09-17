5a49e602fba11da36271f5dd8b3f31d969917931
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2025-09-17 17:37:21 +0530

    Auto-fix for task Task1

--- proposed_fixes/Task1_proposed_fix.py
+++ proposed_fixes/Task1_proposed_fix.py
@@ -5,13 +5,13 @@ display(df)
 # COMMAND ----------
 
 df = spark.table("model.modelllm.weather_data_jul_10_2025")
-filtered_df = df.filter((df.state == "Bihar") & (df.district == "Aurangabad")) 
+filtered_df = df.filter((df.state == "Bihar") & (df.district == "Aurangabad"))
 display(filtered_df)
 
 # COMMAND ----------
 
 df = spark.table("model.modelllm.weather_data_jul_10_2025")
-filtered_df = df.filter((df.state == "Bihar") & (df.district == "Arwal")) 
+filtered_df = df.filter((df.state == "Bihar") & (df.district == "Arwal"))
 display(filtered_df)
 
 # COMMAND ----------
@@ -41,3 +41,5 @@ df_agg = (
 )
 
 display(df_agg)
+
+# COMMAND ----------
